96aaafb219845c532d4ac85f710dd0ce025a0c86
yo <franciscocguerrero@gmail.com>
2022-07-12 19:12:49 +0200

    TS + JEST + REACT configuration setup

---
+++ src/client/components/Cell.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import renderer from 'react-test-renderer';
+import Cell from './Cell';
+import peonwhite from '../assets/peon_blanco.png';
+
+it('shows possible moves when pawn cell 78 hovered', () => {
+    const component = renderer.create(
+        <Cell
+            key={'78'}
+            img={peonwhite}
+            row={7}
+            col={8}
+        />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+})
